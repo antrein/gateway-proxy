18cbc32043ba2d6ca399659356e88ab8a594638e
Yandy <riandyhsn@gmail.com>
2024-05-18 17:41:50 +0700

    feat: add authrization token checker

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module gateway-proxy
 
 go 1.19
+
+require github.com/golang-jwt/jwt/v5 v5.2.1
--- main.go
+++ main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os"
+	"regexp"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
 )
 
 func main() {
 	infra_mode := os.Getenv("INFRA_MODE")
-
+	token_secret := os.Getenv("TOKEN_SECRET")
+	htmlContent := `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Unauthorized</title>
+    </head>
+    <body>
+        <h1>Unauthorized Access</h1>
+        <p>You do not have permission to access this page.</p>
+    </body>
+    </html>
+    `
 	var target string
 
 	if infra_mode == "multi" {
@@ -17,30 +36,87 @@ func main() {
 	} else {
 		target = os.Getenv("SERVICE_URL") // ubah jadi get url data dari db
 	}
+
 	proxyUrl, err := url.Parse(target)
 	if err != nil {
 		panic(err)
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if isValidToken(r.Header.Get("X-Access-Token")) {
+		host := r.Referer()
+		projectID, err := extractProjectID(host)
+		if err != nil {
+			w.Header().Set("Content-Type", "text/html; charset=utf-8")
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte(htmlContent))
+
+		}
+		auth, err := r.Cookie("antrein_authorization")
+		if err != nil {
+			w.Header().Set("Content-Type", "text/html; charset=utf-8")
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte(htmlContent))
+		}
+
+		if isValidToken(auth.Value, token_secret, projectID) {
 			// Masuk ke reverse proxy
 			proxy := httputil.NewSingleHostReverseProxy(proxyUrl)
 			proxy.ServeHTTP(w, r)
 		} else {
-			// Kelempar
+			w.Header().Set("Content-Type", "text/html; charset=utf-8")
 			w.WriteHeader(http.StatusUnauthorized)
-			w.Write([]byte("Unauthorized"))
+			w.Write([]byte(htmlContent))
 		}
 	})
 
 	http.ListenAndServe(":8080", nil)
 }
 
-// isValidToken checks if the provided access token is valid
-func isValidToken(token string) bool {
-	// Logic checker di sini
-	if token == "valid-token" {
+func extractProjectID(url string) (string, error) {
+	re := regexp.MustCompile(`https?://([^.]+)\.antrein\.com`)
+	matches := re.FindStringSubmatch(url)
+	if len(matches) < 2 {
+		return "", fmt.Errorf("URL not registered")
+	}
+	return matches[1], nil
+}
+
+func authorizationCheck(authToken, secret, projectID string) bool {
+	token, err := jwt.Parse(authToken, func(token *jwt.Token) (interface{}, error) {
+		return []byte(secret), nil
+	})
+
+	if err != nil || !token.Valid {
+		return false
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return false
+	}
+
+	if exp, ok := claims["exp"].(float64); ok {
+		if time.Unix(int64(exp), 0).Before(time.Now()) {
+			return false
+		}
+	} else {
+		return false
+	}
+
+	tokenProjectID, ok := claims["project_id"].(string)
+	if !ok {
+		return false
+	}
+
+	if tokenProjectID != projectID {
+		return false
+	}
+
+	return true
+}
+
+func isValidToken(token, secret, projectID string) bool {
+	if authorizationCheck(token, secret, projectID) {
 		return true
 	} else {
 		return false
